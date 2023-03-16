e4c35fca75aadc3975f8f52bf86e33e7b475643b
Veronika <116470467+vilvee@users.noreply.github.com>
2023-03-16 15:49:50 -0400

    day 3 part 2 finished

--- Day 3/Day3/Program.cs
+++ Day 3/Day3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using static System.Console;
 
 namespace Day3
@@ -8,63 +9,95 @@ namespace Day3
         static void Main(string[] args)
         {
             List<string> itemsAll = File.ReadAllLines("items.txt").ToList();
-            var compartment = Populate(itemsAll);
+            /* var compartment = Populate(itemsAll);
+           PART ONE
             List<string> firstCompartment = compartment.Item1; 
             List<string> secondCompartment = compartment.Item2;
+            int results = Points(firstCompartment, secondCompartment);*/
 
-            int results = Points(firstCompartment, secondCompartment);
+            int result = ResultTwo(itemsAll);
 
-            Write(results);
+            Write(result);
 
             ReadKey();
         }
+        //PART TWO
 
-        static (List<string>, List<string>) Populate(List<string> items)
-        {
-            List<string> firstCompartment = new();
-            List<string> secondCompartment = new();
-
-            foreach (string item in items)
-            {
-                firstCompartment.Add(item.Substring(0, item.Length /2));      
-                secondCompartment.Add(item.Substring(item.Length / 2, item.Length - (item.Length / 2)));  
-            }
-
-            return (firstCompartment, secondCompartment);
-        }
-
-        static int Points (List<string> first, List<string> second)
+        static int ResultTwo(List<string> items)
         {
             List<int> pointList = new();
-            for(int i =0; i < first.Count; ++i)
-            { 
-                string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-                string one = first[i];
-                string two = second[i];
+            string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-                foreach(char letter in two)
+            for (int i = 0; i < items.Count; i+=3)
+            {
+            
+                foreach (char letter in letters)
                 {
-                    bool trues = one.Contains(letter.ToString());
+                    bool trueOne = items[i].Contains(letter);
+                    bool trueTwo = items[i + 1].Contains(letter);
+                    bool trueThree = items[i + 2].Contains(letter);
 
-                    if (trues == true)
+                    if (trueOne && trueTwo  && trueThree)
                     {
-                        int index = letters.IndexOf(letter);
-
-                        if (char.IsUpper(letter))
-                            pointList.Add(index+1);
-                        else
-                            pointList.Add(index+1);
-
-                        WriteLine(one);
-                        WriteLine(two);
-                        Write(letter);
-                        Write(index + 1 + "\n");
+                        pointList.Add(letters.IndexOf(letter)+1);
                         break;
-                    } 
+                    }
                 }
+
             }
             int sum = pointList.Sum();
             return sum;
+
+
         }
+
+        /*  //PART ONE
+          static (List<string>, List<string>) Populate(List<string> items)
+          {
+              List<string> firstCompartment = new();
+              List<string> secondCompartment = new();
+
+              foreach (string item in items)
+              {
+                  firstCompartment.Add(item.Substring(0, item.Length /2));      
+                  secondCompartment.Add(item.Substring(item.Length / 2, item.Length - (item.Length / 2)));  
+              }
+
+              return (firstCompartment, secondCompartment);
+          }
+
+          static int Points (List<string> first, List<string> second)
+          {
+              List<int> pointList = new();
+              for(int i =0; i < first.Count; ++i)
+              { 
+                  string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+                  string one = first[i];
+                  string two = second[i];
+
+                  foreach(char letter in two)
+                  {
+                      bool trues = one.Contains(letter.ToString());
+
+                      if (trues == true)
+                      {
+                          int index = letters.IndexOf(letter);
+
+                          if (char.IsUpper(letter))
+                              pointList.Add(index+1);
+                          else
+                              pointList.Add(index+1);
+
+                          WriteLine(one);
+                          WriteLine(two);
+                          Write(letter);
+                          Write(index + 1 + "\n");
+                          break;
+                      } 
+                  }
+              }
+              int sum = pointList.Sum();
+              return sum;
+          }*/
     }
 }
